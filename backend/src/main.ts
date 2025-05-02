1d9f62d7e83cef6ff1a7eb71ef4ceb1a252fc09b
aleksey10832 <akuratov47@gmail.com>
2025-05-03 04:17:47 +0600

    начало адпативной вёрстки

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'http://127.0.0.1:5500',
+    origin: [
+      'http://127.0.0.1:5500',
+      'http://127.0.0.1:8000',
+      'http://192.168.0.14:8000',
+    ],
   });
   app.use('/file', express.static(join(__dirname, '..', 'public')));
   await app.listen(process.env.PORT ?? 3000);
--- frontend/projects/projects.js
+++ frontend/projects/projects.js
@@ -5,16 +5,19 @@ let currentCount = 0;
 let maxPage = document.querySelector("#max-page");
 
 const projects = document.querySelector("#projects")
-async function Update(currentcount){
-
-    maxPage.textContent =  Math.ceil(((await (await fetch(`http://localhost:3000/projects`)).json()).count) / 3).toString().padStart(2, '0')
-    const respons = await (await fetch(`http://localhost:3000/projects?page=${currentCount + 1}`)).json()
+async function Update(currentcount){    
+    try{
+        maxPage.textContent =  Math.ceil(((await (await fetch(`http://localhost:3000/projects`)).json()).count) / 3).toString().padStart(2, '0')
+        const respons = await (await fetch(`http://localhost:3000/projects?page=${currentCount + 1}`)).json()
+    } catch(err){
+        maxPage.textContent = err
+    }
     console.log(respons)
     let stroka = ""
     respons.forEach(element => {
         if(element){
             stroka +=`<li class="project"> 
-                    <img width="85%" height="280px" src="http://localhost:3000/public${element.images[0].path}" alt="404">
+                    <img width="85%" height="280px" src="http://localhost:3000/file/${element.images[0].fileName}" alt="404">
                     <h1 class="text-head-1">${element.name}</h1>
                     <p>${element.description}</p>
                     <button>Узнать больше <img src="imgs/arrow-right.png"></button>
